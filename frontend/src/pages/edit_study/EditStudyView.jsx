d00644f285cf43ac2748d399d23c8d68b7ba684b
FiveGoblins <andresbasaezv@gmail.com>
2025-05-14 19:41:36 -0400

    feat: PDF support test

--- frontend/src/pages/edit_study/EditStudyView.jsx
+++ frontend/src/pages/edit_study/EditStudyView.jsx
@@ -1,15 +1,27 @@
-import { useParams } from "react-router";
-import StudyForm from "./studyForm";
+"use client"
+
+import { useParams } from "react-router"
+import StudyForm from "./studyForm"
 
 function EditStudyView() {
-    const params = useParams();
+  const params = useParams()
+  const fileUrl = "/Biology-and-Behavior-2016.pdf"
 
-    return (
-        <div className="flex flex-col h-full bg-violet-50">
-            <div className="flex-grow p-4 overflow-auto">
-                <StudyForm studyid={params.studyid} />
-            </div>
+  function PdfEmbed({ url }) {
+    return <iframe src={url} width="100%" height="100%" style={{ border: "none" }} title="PDF Viewer" />
+  }
+
+  return (
+    <div className="flex flex-row h-screen bg-violet-50">
+        <div className="flex-grow overflow-auto h-full">
+            <StudyForm studyid={params.studyid} />
         </div>
-    );
+        <div className=" w-3/4 h-full">
+            <PdfEmbed url={fileUrl} />
+        </div>
+      
+    </div>
+  )
 }
-export default EditStudyView;
+
+export default EditStudyView
