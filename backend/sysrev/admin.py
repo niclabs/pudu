2eab00da68f14c7e2ab7e67474d84f0a1cdbe551
ignacio <ignacio.gajardo.s@ug.uchile.cl>
2026-01-06 16:24:02 -0300

    Update admin.py

--- backend/sysrev/admin.py
+++ backend/sysrev/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+
+
+from django.contrib import admin 
+from .models import Review, Study, Author, Tag
+  
+admin.site.register(Review)
+admin.site.register(Study)
+admin.site.register(Author)
+admin.site.register(Tag)
