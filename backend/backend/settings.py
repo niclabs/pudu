2d06cedc7422d8294a6caa413c2d05c182f1f148
ignacio <ignacio.gajardo.s@ug.uchile.cl>
2026-01-08 11:10:33 -0300

    refactor: Tiempo de vida del token (closes #47)
    cambio de 1 dia a 5 min

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -121,7 +121,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),  # <-- esto es pesimo, implementar un sistema de refresco de tokens para no usar tokens de acceso tan largos
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),  
 }
 # Internationalization
