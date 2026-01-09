9279466c7789bd5e9ce9428eb29e69f23d4e4c5f
ignacio <ignacio.gajardo.s@ug.uchile.cl>
2026-01-09 18:37:38 -0300

    feat: send email function

--- backend/sysrev/models.py
+++ backend/sysrev/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.timezone import now
 
+from django_rest_passwordreset.signals import reset_password_token_created
+from django.dispatch import receiver 
+from django.urls import reverse 
+from django.template.loader import render_to_string
+from django.core.mail import EmailMultiAlternatives
+from django.utils.html import strip_tags
 
 class Review(models.Model):
     name = models.CharField(max_length=255)
@@ -71,3 +77,30 @@ class Tag(models.Model):
 
 
 
+@receiver(reset_password_token_created)
+def password_reset_token_created(reset_password_token, *args, **kwargs):
+    sitelink = "http://localhost:5173/"
+    token = "{}".format(reset_password_token.key)
+    full_link = str(sitelink)+str("password-reset/")+str(token)
+
+    print(token)
+    print(full_link)
+
+    context = {
+        'full_link': full_link,
+        'email_adress': reset_password_token.user.email
+    }
+
+    html_message = render_to_string("email.html", context=context)
+    plain_message = strip_tags(html_message)
+
+    msg = EmailMultiAlternatives(
+        subject = "Request for resetting password for {title}".format(title=reset_password_token.user.email), 
+        body=plain_message,
+        from_email = "sender@example.com", 
+        to=[reset_password_token.user.email]
+    )
+
+    msg.attach_alternative(html_message, "text/html")
+    msg.send()
+    
